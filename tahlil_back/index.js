84cbdb520726a2f0090a442be41f03e78c383db3
dorrin-sot <dorrinsotoudeh123@gmail.com>
2024-05-16 08:00:23 +0330

    server setup register added

---
+++ tahlil_back/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
+const bodyParser = require('body-parser')
+
+const {Client} = require('pg')
+
+const auth_requests = require('./routes/user/auth');
+const healthcheck_request = require('./routes/health-check');
+
+
+const db_client = new Client({
+    user: process.env.pg_user,
+    password: process.env.pg_password,
+    host: process.env.pg_host,
+    port: process.env.pg_port,
+    database: process.env.pg_database,
+});
+
+db_client
+    .connect()
+    .then(() => console.log('Connected to PostgreSQL database'))
+    .catch((err) => console.error('Error connecting to PostgreSQL database', err));
+
+const app = express();
+const port = process.env.server_port;
+const jsonParser = bodyParser.json()
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+healthcheck_request(app)
+auth_requests(app, db_client, jsonParser);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
---
+++ tahlil_back/routes/user/auth.js
@@ -0,0 +1,77 @@
+function auth_requests(app, db, jsonParser) {
+    /**
+     * @swagger
+     * /auth/register:
+     *   post:
+     *     summary: Register
+     *     description: Register.
+     *     requestBody:
+     *       description: Optional description in *Markdown*
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *               ssid: string
+     *               first_name: string
+     *               last_name: string
+     *               username: string
+     *               password: string
+     *               repeat_password: string
+     *               medical_id: string
+     *               user_type: string
+     *             example:
+     *               ssid: 1234567890
+     *               first_name: fname
+     *               last_name: lname
+     *               username: usern
+     *               password: passpass123
+     *               repeat_password: passpass123
+     *               medical_id: 12345
+     *               user_type: doctor
+     *     responses:
+     *       201:
+     *         description: Successful register.
+     *       401:
+     *         description: An error occurred during register. Messages in body.
+     *
+     */
+    console.log(process.env.pg_password, process.env.pg_user)
+    app.post('/auth/register', jsonParser, async function (req, res) {
+        const {ssid, first_name, last_name, username, password, repeat_password, medical_id, user_type} = req.body;
+        const {rowCount} = await db.query(`select * from public."user" where ssid='${ssid}'`);
+        if (rowCount > 0) {
+            res.status(401).send('SSID should be Unique.')
+        } else if (password !== repeat_password) {
+            res.status(401).send('Password and Repeat Password should match.')
+        } else if (password.length < 8) {
+            res.status(401).send('Password should be at least 8 characters long.')
+        } else if (!['doctor', 'patient', 'referrer'].includes(user_type)) {
+            res.status(401).send("User Type should be between 'doctor', 'patient', 'referrer'.")
+        } else if (user_type === 'doctor' && (medical_id || '').length == 0) {
+            res.status(401).send("Medical ID is required for doctors.")
+        } else {
+            const values = [ssid, first_name, last_name, username, password];
+            if (user_type === 'doctor') {
+                values.push(medical_id);
+                const {rowCount} = await db.query(`select * from public."doctor" where medical_id='${medical_id}'`);
+                if (rowCount > 0) {
+                    res.status(401).send('Medical ID should be Unique.')
+                    return
+                }
+            } else if (user_type === 'referrer') {
+                values.push(true) // referrer is verified by default
+            }
+
+            res.status(201).json(await db.query({
+                text: `insert into public.${user_type} ` +
+                    `(ssid, first_name, last_name, username, password${user_type == 'doctor' ? ", medical_id" : ""}${user_type == 'referrer' ? ", is_verified" : ""}) ` +
+                    `values ($1, $2, $3, $4, $5${user_type != 'patient' ? ", $6" : ""})`,
+                values: values,
+            }));
+        }
+    })
+}
+
+module.exports = auth_requests;
