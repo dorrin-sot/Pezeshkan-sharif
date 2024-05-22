1818920c582f802c884645c8a5f462ec1eed6b12
dorrin-sot <dorrinsotoudeh123@gmail.com>
2024-05-22 20:41:45 +0330

    old code is in main-old branch. deleted Front folder

--- Front/src/back-requests/auth.js
+++
@@ -1,8 +0,0 @@
-import { post_request } from "./base";
-
-export function register_req(ssid, first_name, last_name, password, repeat_password, user_type, medical_id = null) {
-    return post_request(
-        '/auth/register',
-        { ssid, first_name, last_name, password, repeat_password, user_type, medical_id }
-    );
-}
