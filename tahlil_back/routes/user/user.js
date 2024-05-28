99386ad7f59f1564a87af02574600999cb7f9bdc
dorrin-sot <dorrinsotoudeh123@gmail.com>
2024-05-28 20:04:15 +0330

    fix small back bug

--- tahlil_back/routes/user/user.js
+++ tahlil_back/routes/user/user.js
@@ -18,6 +18,7 @@ function user_requests(app, db, jsonParser) {
         let {token} = req.cookies;
 
         const {rows} = await db.query(`select * from public."login_token" where token='${token}' order by created_at desc limit 1`);
+        if (rows.length === 0) return res.status(401).send('Invalid Token!')
         const {ssid, created_at, user_type} = rows[0]
 
         if (!validateJwtToken(token)) {
