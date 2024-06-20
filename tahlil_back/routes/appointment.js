0433f9f0a90880b0d396070d9e932a9bc1fd0611
dorrin-sot <dorrinsotoudeh123@gmail.com>
2024-06-20 19:43:39 +0330

    added GET /appointment/:id/images

--- tahlil_back/routes/appointment.js
+++ tahlil_back/routes/appointment.js
@@ -1,3 +1,5 @@
+// noinspection DuplicatedCode
+
 const {validateJwtToken} = require('../utils/jwt');
 const {auth_midware} = require("./middlewares/auth");
 const {pfp_storage, opg_images_storage} = require("../utils/files");
@@ -268,7 +270,7 @@ function appointment_requests(app, db, jsonParser) {
      *                     format: binary
      *     responses:
      *       201:
-     *         description: Appointment returned as json
+     *         description: Images uploaded successfully!
      *       404:
      *         description: Appointment not found
      *       401:
@@ -293,6 +295,61 @@ function appointment_requests(app, db, jsonParser) {
             }
             res.status(200).send('Images Uploaded successfully!');
         });
+
+    /**
+     * @swagger
+     * /appointment/{id}/images:
+     *   get:
+     *     summary: Get appointment images
+     *     parameters:
+     *       - in: path
+     *         name: id
+     *         schema:
+     *           type: integer
+     *         required: true
+     *         description: The ID of the appointment
+     *     responses:
+     *       200:
+     *         description: Appointment returned as json
+     *       404:
+     *         description: Appointment not found
+     *       401:
+     *         description: Unauthorized access. Try logging in or refreshing token.
+     *
+     */
+    app.get('/appointment/:id/images', async function (req, res) {
+        let {token} = req.cookies;
+
+        const {rows} = await db.query(`select * from public."login_token" where token='${token}' order by created_at desc limit 1`);
+        if (rows.length === 0) return res.status(401).send('Invalid Token!');
+        const {ssid, created_at, user_type} = rows[0]
+
+        if (!validateJwtToken(token)) {
+            res.status(401).send('Invalid Token!')
+        } else if ((new Date()).getTime() - created_at.getTime() >= process.env.cookie_max_age) {
+            res.status(401).send('Old Token! Send a GET /auth/refresh request and try again.')
+        } else {
+            const {id: appointment_id} = req.params;
+            let type = 'doctor'
+            let {rows: appointment, rowCount} = await db.query({text: `select * from public."${type}_appointment_v1" where id=$1`, values: [parseInt(appointment_id)]})
+                .catch(console.log)
+
+            if (rowCount === 0) {
+                type = 'imaging_center'
+                const result = await db.query({text: `select * from public."${type}_appointment_v1" where id=$1`, values: [parseInt(appointment_id)]})
+                    .catch(console.log)
+                appointment = result['rows']
+                rowCount = result['rowCount']
+            }
+
+            if (rowCount === 0 || ![appointment[0]['patient'], appointment[0]['doctor'], appointment[0]['imaging_center_name']].includes(ssid))
+                return res.status(404).send('Appointment not found!')
+
+            const {rows: list} = await db.query({text: 'select image_link from public."opg_image" where appointment=$1', values: [parseInt(appointment_id)]})
+                .catch(console.log)
+            res.status(200).json(list.map((i) => i['image_link']));
+        }
+    });
 }
 
 module.exports = appointment_requests;
