0b29c0e2288f19b4e9ee1ecdaeb0ae605b01dc5c
dorrin-sot <dorrinsotoudeh123@gmail.com>
2024-06-25 13:16:21 +0330

    fix scroll in auth page

--- tahlil_front/lib/pages/auth.dart
+++ tahlil_front/lib/pages/auth.dart
@@ -41,139 +41,141 @@ class _AuthPageState extends State<AuthPage> {
           borderRadius: BorderRadius.circular(10),
           boxShadow: const [BoxShadow(color: Colors.black45, blurRadius: 2)],
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(50),
-          child: SizedBox(
-            width: _isLogin ? 350 : 475,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Row(
-                  children: [
-                    const Expanded(child: Divider()),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: Text(
-                        _isLogin ? 'Login' : 'Register',
-                        style: Theme.of(context).textTheme.headlineSmall,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(50),
+            child: SizedBox(
+              width: _isLogin ? 350 : 475,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    children: [
+                      const Expanded(child: Divider()),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(
+                          _isLogin ? 'Login' : 'Register',
+                          style: Theme.of(context).textTheme.headlineSmall,
+                        ),
                       ),
-                    ),
-                    const Expanded(child: Divider()),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                SegmentedButton<UserType>(
-                  segments: (_isLogin
-                          ? [UserType.referrer, UserType.imaging_center]
-                          : UserType.values)
-                      .map((ut) => ButtonSegment(
-                            value: ut,
-                            label: FittedBox(
-                              child: Text(
-                                _isLogin && ut == UserType.referrer
-                                    ? 'User'
-                                    : '$ut',
+                      const Expanded(child: Divider()),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  SegmentedButton<UserType>(
+                    segments: (_isLogin
+                            ? [UserType.referrer, UserType.imaging_center]
+                            : UserType.values)
+                        .map((ut) => ButtonSegment(
+                              value: ut,
+                              label: FittedBox(
+                                child: Text(
+                                  _isLogin && ut == UserType.referrer
+                                      ? 'User'
+                                      : '$ut',
+                                ),
                               ),
-                            ),
-                          ))
-                      .toList(),
-                  showSelectedIcon: false,
-                  selected: <UserType>{_userType},
-                  onSelectionChanged: (newUserType) =>
-                      setState(() => _userType = newUserType.last),
-                ),
-                const SizedBox(height: 10),
-                if (_userType == UserType.imaging_center)
-                  CustomTextField(
-                    icon: const FaIcon(FontAwesomeIcons.idCard),
-                    label: 'Name',
-                    required: true,
-                    controller: _nameController,
-                  )
-                else
-                  CustomTextField(
-                    icon: const FaIcon(FontAwesomeIcons.idCard),
-                    label: 'SSID',
-                    required: true,
-                    controller: _ssidController,
+                            ))
+                        .toList(),
+                    showSelectedIcon: false,
+                    selected: <UserType>{_userType},
+                    onSelectionChanged: (newUserType) =>
+                        setState(() => _userType = newUserType.last),
                   ),
-                if (!_isLogin && _userType != UserType.imaging_center) ...[
-                  CustomTextField(
-                    icon: const Icon(Icons.abc),
-                    label: 'First Name',
-                    required: true,
-                    controller: _firstNameController,
-                  ),
-                  CustomTextField(
-                    icon: const Icon(Icons.abc),
-                    label: 'Last Name',
-                    required: true,
-                    controller: _lastNameController,
-                  ),
-                ],
-                CustomTextField(
-                  icon: const Icon(Icons.password),
-                  label: 'Password',
-                  required: true,
-                  controller: _passwordController,
-                  obscureText: !_passwordShown,
-                  toggleObscureText: () =>
-                      setState(() => _passwordShown = !_passwordShown),
-                  helperText: 'At least 8 characters',
-                ),
-                if (!_isLogin) ...[
+                  const SizedBox(height: 10),
+                  if (_userType == UserType.imaging_center)
+                    CustomTextField(
+                      icon: const FaIcon(FontAwesomeIcons.idCard),
+                      label: 'Name',
+                      required: true,
+                      controller: _nameController,
+                    )
+                  else
+                    CustomTextField(
+                      icon: const FaIcon(FontAwesomeIcons.idCard),
+                      label: 'SSID',
+                      required: true,
+                      controller: _ssidController,
+                    ),
+                  if (!_isLogin && _userType != UserType.imaging_center) ...[
+                    CustomTextField(
+                      icon: const Icon(Icons.abc),
+                      label: 'First Name',
+                      required: true,
+                      controller: _firstNameController,
+                    ),
+                    CustomTextField(
+                      icon: const Icon(Icons.abc),
+                      label: 'Last Name',
+                      required: true,
+                      controller: _lastNameController,
+                    ),
+                  ],
                   CustomTextField(
                     icon: const Icon(Icons.password),
-                    label: 'Repeat Password',
+                    label: 'Password',
                     required: true,
-                    controller: _repeatPasswordController,
+                    controller: _passwordController,
                     obscureText: !_passwordShown,
                     toggleObscureText: () =>
                         setState(() => _passwordShown = !_passwordShown),
+                    helperText: 'At least 8 characters',
                   ),
-                  if (_userType != UserType.referrer)
+                  if (!_isLogin) ...[
                     CustomTextField(
-                      icon: const Icon(Icons.verified_user),
-                      label: 'Referrer',
+                      icon: const Icon(Icons.password),
+                      label: 'Repeat Password',
                       required: true,
-                      controller: _referrerController,
+                      controller: _repeatPasswordController,
+                      obscureText: !_passwordShown,
+                      toggleObscureText: () =>
+                          setState(() => _passwordShown = !_passwordShown),
                     ),
-                  if (_userType == UserType.doctor)
-                    CustomTextField(
-                      icon: const Icon(Icons.medical_information),
-                      label: 'Medical ID',
-                      required: true,
-                      controller: _medicalIdController,
-                      hint: 'e.g. 12345',
-                    )
-                ],
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      _isLogin
-                          ? 'Don\'t have an account?'
-                          : 'Already have an account?',
-                    ),
-                    const SizedBox(width: 5),
-                    TextButton(
-                      onPressed: () => setState(() {
-                        if (_userType != UserType.imaging_center) {
-                          _userType = UserType.referrer;
-                        }
-                        _isLogin = !_isLogin;
-                      }),
-                      child: Text(_isLogin ? 'Register Here!' : 'Login Here!'),
-                    )
+                    if (_userType != UserType.referrer)
+                      CustomTextField(
+                        icon: const Icon(Icons.verified_user),
+                        label: 'Referrer',
+                        required: true,
+                        controller: _referrerController,
+                      ),
+                    if (_userType == UserType.doctor)
+                      CustomTextField(
+                        icon: const Icon(Icons.medical_information),
+                        label: 'Medical ID',
+                        required: true,
+                        controller: _medicalIdController,
+                        hint: 'e.g. 12345',
+                      )
                   ],
-                ),
-                const SizedBox(height: 25),
-                FilledButton(
-                  onPressed: _isLogin ? _login : _register,
-                  child: Text(_isLogin ? 'Login' : 'Register'),
-                )
-              ],
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        _isLogin
+                            ? 'Don\'t have an account?'
+                            : 'Already have an account?',
+                      ),
+                      const SizedBox(width: 5),
+                      TextButton(
+                        onPressed: () => setState(() {
+                          if (_userType != UserType.imaging_center) {
+                            _userType = UserType.referrer;
+                          }
+                          _isLogin = !_isLogin;
+                        }),
+                        child: Text(_isLogin ? 'Register Here!' : 'Login Here!'),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 25),
+                  FilledButton(
+                    onPressed: _isLogin ? _login : _register,
+                    child: Text(_isLogin ? 'Login' : 'Register'),
+                  )
+                ],
+              ),
             ),
           ),
         ),
